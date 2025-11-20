12ebb5704e1876adb571aa5e9d211b5eea7648c3
codeguyakash <codeguyakash@gmail.com>
2025-11-21 01:46:19 +0530

    fix: update SECRET_KEY handling and correct typos in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user");
 const bcyrpt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = "NOTESAPI";
+const SECRET_KEY = process.env.SECRET_KEY || "YourSecretKey";
 
 const signup = async (req, res) => {
   //existing user check
@@ -20,14 +20,15 @@ const signup = async (req, res) => {
       password: hashedPassword,
       username: username,
     });
-    //token genrate
+    //token generate
     const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
     res.status(201).json({ user: result, token: token });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ massege: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
+
 const signin = async (req, res) => {
   const { email, password } = req.body;
 
@@ -37,20 +38,24 @@ const signin = async (req, res) => {
     if (!existingUser) {
       return res.status(404).json({ message: "User Not Found" });
     }
-    //bcyrpted password match condition
+    //bcrypted password match condition
     const matchPassword = await bcyrpt.compare(password, existingUser.password);
     if (!matchPassword) {
       return res.status(404).json({ message: "Invalid Credentials" });
     }
-    //token genrate
+    //token generate
     const token = jwt.sign(
       { email: existingUser.email, id: existingUser._id },
       SECRET_KEY
     );
-    res.status(200).json({ user: existingUser, token: token });
+
+    const user = existingUser.toObject();
+    delete user.password;
+
+    res.status(200).json({ user, token });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ massege: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
